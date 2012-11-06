4a7d54f44c6057a4ebcf8629348fe681892adebe
Ilja <pman89@ya.ru>
2012-11-06 11:35:44 +0400

    Update db/seeds.rb
    fixed seeding to database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,18 @@ puts 'All Users and Pages destroyed!'
   3.times do |i|
     puts '-> new book'
     book = u.pages.new
-    book.title       = "Book: #{Faker::Lorem.paragraph 1}"
-    book.raw_content = "From: author: #{Faker::Lorem.paragraph 6}"
+    book.title           = "Book: #{Faker::Lorem.paragraph 1}"
+    book.raw_content     = "From: author: #{Faker::Lorem.paragraph 6}"
+    book.raw_description = "From: #{Faker::Lorem.paragraph 3}"
     book.save!
 
     10.times do
       puts '--> new chapter'
 
       chapter = u.pages.new
-      chapter.title       = "Chapter: #{Faker::Lorem.paragraph 1}"
-      chapter.raw_content = "Chapter description: #{Faker::Lorem.paragraph 6}"
+      chapter.title           = "Chapter: #{Faker::Lorem.paragraph 1}"
+      chapter.raw_content     = "Chapter content: #{Faker::Lorem.paragraph 6}"
+      chapter.raw_description = "Chapter description: #{Faker::Lorem.paragraph 3}"
       chapter.save!
       chapter.move_to_child_of book
 
@@ -33,8 +35,9 @@ puts 'All Users and Pages destroyed!'
         puts '---> new page'
 
         page = u.pages.new
-        page.title       = Faker::Lorem.paragraph 1
-        page.raw_content = Faker::Lorem.paragraph 6
+        page.title           = Faker::Lorem.paragraph 1
+        page.raw_content     = Faker::Lorem.paragraph 6
+        page.raw_description = Faker::Lorem.paragraph 3
         page.save!
         page.move_to_child_of chapter
       end
